41ee4d765dc4bd524626b42eadbc9062da73b76b
LittleJoee <josephowolabi428@gmail.com>
2026-02-20 22:55:19 +0100

    audio effects i couldnt test rn so branch

--- game.py
+++ game.py
@@ -44,9 +44,22 @@ class Game:
             'particles/particle': Animation(load_images('particles/particle'), img_dur=6, loop=False),
             'gun': load_image('gun.png'),
             'projectile': load_image('projectile.png'),
+            }
 
+
+        self.sfx = {
+            'jump': pygame.mixer.Sound('data/sfx/jump.wav'),
+            'dash': pygame.mixer.Sound('data/sfx/dash.wav'),
+            'hit': pygame.mixer.Sound('data/sfx/hit.wav'),
+            'shoot': pygame.mixer.Sound('data/sfx/shoot.wav'),
+            'ambience': pygame.mixer.Sound('data/sfx/ambience.wav'),
         }
 
+        self.sfx['jump'].set_volume(0.7)
+        self.sfx['dash'].set_volume(0.3)
+        self.sfx['hit'].set_volume(0.8)
+        self.sfx['shoot'].set_volume(0.4)
+        self.sfx['ambience'].set_volume(0.2)
         self.clouds = Clouds(self.assets['clouds'], count=16)
 
         self.player = Player(self, [50, 50], [8, 15])
@@ -83,6 +96,12 @@ class Game:
 
 
     def run(self):
+        pygame.mixer.music.load('data/music.wav')
+        pygame.mixer.music.set_volume(0.5)
+        pygame.music.play(-1)
+
+        self.ambience.play(-1)
+
         while True:
             self.display.fill((0, 0, 0, 0))
             self.display_2.blit(self.assets['background'], (0, 0))
@@ -145,6 +164,7 @@ class Game:
                     if self.player.rect().collidepoint(projectile[0]):
                         self.projectiles.remove(projectile)
                         self.dead += 1
+                        self.sfx['hit'].play()
                         self.screenshake = max(16, self.screenshake)
                         # player hit sparks
                         for i in range(30):
@@ -187,7 +207,8 @@ class Game:
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = True
                     if event.key == pygame.K_UP:
-                        self.player.jump()
+                        if self.player.jump():
+                            self.sfx['jump'].play()
                     if event.key == pygame.K_x:
                         self.player.dash()
                 if event.type == pygame.KEYUP:
